d24ec24862c1e4aab488d1964adc3e6ee1d0dc46
CVC_DM\mtzcpd1268 <rafaeldocarmo@ext.cvc.com.br>
2018-10-02 11:07:10 -0300

    Preparação para 2 cartões comentada

--- popup.js
+++ popup.js
@@ -8,6 +8,7 @@
   let productType = checkout && checkout.appConfig.productType;
 
   let fillFieldsInterval = setInterval(function () {
+    console.log("Iniciado");
     if (checkout && checkout.data && checkout.model) {
       if (checkout.data.modalities && checkout.data.modalities.length) {
         fillFields();
@@ -18,42 +19,46 @@
 
   function fillFields() {
     if (checkout && checkout.data && checkout.model) {
-      let a = 97;
-
+      let a = 65;
       _.each(checkout.data.customerGroups, cg => {
         _.each(cg, c => {
           Ember.setProperties(c, {
-            firstName: 'maria',
-            lastName: 'silva' + String.fromCharCode(a++),
+            firstName: 'Maria',
+            lastName: `Silva ${String.fromCharCode(a++)}`,
             birthday: moment(c.referenceDate).subtract(c.range ? c.range[Math.floor(Math.random() * c.range.length)] : c.age, 'year').format('DD/MM/YYYY')
           });
         });
       });
 
-      _.each(checkout.data.modalities, m => {
-        if (m.isSelected) {
-          Ember.setProperties(m, {
-            number: '4242 4242 4242 4242',
-            expirationDate: moment().add(5, 'years').format('MM/YYYY'),
-            name: 'maria silva',
-            securityCode: '123'
-          });
-        }
-      });
+      // _.each(checkout.data.payers, payer => {
+        Ember.setProperties(checkout.data.payer, {
+          number: '546',
+          cep: '08345-000',
+          state: 'MG',
+          address: {
+            cityName: 'Florinda',
+            street: 'Rua nave mãe',
+            district: 'Albertina'
+          },
+          birthday: moment().subtract(20, 'years').format('DD/MM/YYYY'),
+          phone: '11956561122',
+          cpf: '09606988686'
+        });     
+      // });
+      
+      if (checkout.data.modalities && checkout.data.modalities.length) {
+        _.each(checkout.data.modalities, m => {
+          if (m.isSelected) {
+            Ember.setProperties(m, {
+              number: '4242 4242 4242 4242',
+              expirationDate: moment().add(5, 'years').format('MM/YYYY'),
+              name: 'maria silva',
+              securityCode: '123'
+            });
+          }
+        });
+      }
 
-      Ember.setProperties(checkout.data.payer, {
-        number: '546',
-        cep: '08345-000',
-        state: 'MG',
-        address: {
-          cityName: 'Florinda',
-          street: 'Rua nave mãe',
-          district: 'Albertina'
-        },
-        birthday: moment().subtract(20, 'years').format('DD/MM/YYYY'),
-        phone: '11956561122',
-        cpf: '09606988686'
-      });
 
       Ember.setProperties(checkout.data.voucher, {
         email: 'timesite@cvc.com.br',
